795cb18f2274657704313be82a8c316462c3d330
tothadi <Oakerator66>
2020-02-14 09:45:39 +0100

    charts

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { ResultService } from '../result.service';
 import { AuthenticationService, UserDetails } from '../authentication.service';
-import * as $ from 'jquery';
+import { Chart } from 'chart.js/dist/Chart.bundle';
+import 'chartjs-plugin-datalabels';
+import 'moment';
+import 'chartjs-plugin-zoom';
 
 @Component({
   selector: 'app-charts',
@@ -9,7 +12,7 @@ import * as $ from 'jquery';
   styleUrls: ['./charts.component.css']
 })
 
-export class ChartsComponent implements OnInit {
+export class ChartsComponent implements OnInit, AfterViewInit {
 
   details: UserDetails;
 
@@ -17,122 +20,17 @@ export class ChartsComponent implements OnInit {
     username: ''
   };
 
-  public chartType: string = 'line';
-
-  public chartDatasets: Array<any> = [
-    {
-      data: [], label: 'Level 1'
-    },
-    { data: [], label: 'Level 2' },
-    { data: [], label: 'Level 3' },
-    { data: [], label: 'Level 4' }
-  ];
-
-  public chartLabels: Array<any> = [
-
-  ];
-
-  public chartColors: Array<any> = [
-
-  ];
-
-  public chartOptions: any = {
-    responsive: true,
-    title: {
-      display: true,
-      text: 'Performance Chart',
-      fontColor: 'whitesmoke',
-      fontSize: 16
-    },
-    legend: {
-      labels: {
-        fontColor: "whitesmoke",
-        fontSize: 12
-    }
-    },
-    animation: {
-      easing: 'easeInOutBack'
-    },
-    scales: {
-      xAxes: [{
-        type: 'time',
-        time: {
-          parser: 'timeFormat',
-          tooltipFormat: 'YYYY.MM.DD HH:mm',
-          displayFormats: {
-            'day': 'YYYY.MM.DD'
-          }
-        },
-        display: true,
-        distribution: 'linear',
-        scaleLabel: {
-          display: true,
-          labelString: 'date',
-          fontColor: 'whitesmoke',
-          fontSize: 12
-        },
-        ticks: {
-          source: 'auto',
-          fontColor: 'whitesmoke',
-          fontSize: 10
-        }
-      }],
-      yAxes: [{
-        type: 'linear',
-        display: true,
-        scaleLabel: {
-          display: true,
-          labelString: 'points',
-          fontColor: 'whitesmoke',
-          fontSize: 12
-        },
-        ticks: {
-          fontColor: 'whitesmoke',
-          fontSize: 10
-        }
-      }]
-    },
-    plugins: {
-      zoom: {
-        pan: {
-          enabled: true,
-          mode: 'x',
-          rangeMin: {
-            x: 3600
-          },
-          rangeMax: {
-            x: null,
-            y: null
-          },
-          onPan: function ({ chart }) { console.log(`I'm panning!!!`); },
-          onPanComplete: function ({ chart }) { console.log(`I was panned!!!`); }
-        },
-        zoom: {
-          enabled: true,
-          drag: true,
-          mode: 'x',
-          speed: 0.05,
-          rangeMin: {
-            x: null,
-            y: null
-          },
-          rangeMax: {
-            x: null,
-            y: null
-          },
-          onZoom: function ({ chart }) { console.log(`I'm zooming!!!`); },
-          onZoomComplete: function ({ chart }) { console.log(`I was zoomed!!!`); }
-        }
-      },
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-        font: {
-          size: 20,
-        }
-      }
-    }
-  };
+  canvas: any;
+  ctx: any;
+  chart = [];
+  chartData1 = [];
+  chartData2 = [];
+  chartData3 = [];
+  chartData4 = [];
+  gradientFill1;
+  gradientFill2;
+  gradientFill3;
+  gradientFill4;
 
   public chartClicked(e: any): void { }
   public chartHovered(e: any): void { }
@@ -142,53 +40,6 @@ export class ChartsComponent implements OnInit {
   }
 
   ngOnInit() {
-    setTimeout(() => {
-    let canvas = $('#myCanvas').get(0).getContext('2d');
-
-    var gradientFill1 = canvas.createLinearGradient(0, 0, 0, 300);
-    gradientFill1.addColorStop(0, 'rgba(128, 182, 244, 0.8)');
-    gradientFill1.addColorStop(.3, 'rgba(128, 182, 244, 0.6)');
-    gradientFill1.addColorStop(1, 'rgba(0, 212, 255, 0)');
-
-    var gradientFill2 = canvas.createLinearGradient(0, 0, 0, 300);
-    gradientFill2.addColorStop(0, 'rgba(134, 7, 7, 0.8)');
-    gradientFill2.addColorStop(.3, 'rgba(163, 5, 31, 0.6)');
-    gradientFill2.addColorStop(1, 'rgba(255, 0, 108, 0)');
-
-    var gradientFill3 = canvas.createLinearGradient(0, 0, 0, 300);
-    gradientFill3.addColorStop(0, 'rgba(6, 48, 2, 0.8)');
-    gradientFill3.addColorStop(.3, 'rgba(7, 103, 21, 0.6)');
-    gradientFill3.addColorStop(1, 'rgba(0, 255, 124, 0)');
-
-    var gradientFill4 = canvas.createLinearGradient(0, 0, 0, 300);
-    gradientFill4.addColorStop(0, 'rgba(208, 200, 24, 0.8)');
-    gradientFill4.addColorStop(.3, 'rgba(192, 195, 37, 0.6)');
-    gradientFill4.addColorStop(1, 'rgba(255, 231, 0, 0)');
-    this.chartColors = [
-      {
-        backgroundColor: gradientFill1,
-        borderColor: 'rgba(128, 182, 244, 0.8)',
-        borderWidth: 2,
-      },
-      {
-        backgroundColor: gradientFill2,
-        borderColor: 'rgba(134, 7, 7, 0.8)',
-        borderWidth: 2,
-      },
-      {
-        backgroundColor: gradientFill3,
-        borderColor: 'rgba(6, 48, 2, 0.8)',
-        borderWidth: 2,
-      },
-      {
-        backgroundColor: gradientFill4,
-        borderColor: 'rgba(208, 200, 24, 0.8)',
-        borderWidth: 2,
-      }
-    ];
-    }, 2000);
-    
-
 
     this.auth.profile().subscribe(user => {
       this.details = user;
@@ -210,29 +61,25 @@ export class ChartsComponent implements OnInit {
               x: new Date(results[i].date),
               y: results[i].points
             }
-            this.chartDatasets[0].data.push(result);
-            //var point = new PointConstructor(new Date(results[i].date), results[i].points);
-            //this.chartDatasets[0].data.push(point);
-            //console.log(point);
-            // this.chartLabels[0].data.push(date.format(new Date(results[i].date), 'YYYY.MM.DD'));
+            this.chartData1.push(result);
           } else if (results[i].level === 2) {
             let result = {
               x: new Date(results[i].date),
               y: results[i].points
             }
-            this.chartDatasets[1].data.push(result);
+            this.chartData2.push(result);
           } else if (results[i].level === 3) {
             let result = {
               x: new Date(results[i].date),
               y: results[i].points
             }
-            this.chartDatasets[2].data.push(result);
+            this.chartData3.push(result);
           } else {
             let result = {
               x: new Date(results[i].date),
               y: results[i].points
             }
-            this.chartDatasets[3].data.push(result);
+            this.chartData4.push(result);
           }
 
         }
@@ -244,6 +91,150 @@ export class ChartsComponent implements OnInit {
       console.error(err);
     });
 
+
+  }
+
+  ngAfterViewInit() {
+
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.gradientFill1 = this.ctx.createLinearGradient(0, 0, 0, 300);
+    this.gradientFill1.addColorStop(0, 'rgba(128, 182, 244, 0.8)');
+    this.gradientFill1.addColorStop(.3, 'rgba(128, 182, 244, 0.6)');
+    this.gradientFill1.addColorStop(1, 'rgba(0, 212, 255, 0)');
+
+    this.gradientFill2 = this.ctx.createLinearGradient(0, 0, 0, 300);
+    this.gradientFill2.addColorStop(0, 'rgba(134, 7, 7, 0.8)');
+    this.gradientFill2.addColorStop(.3, 'rgba(163, 5, 31, 0.6)');
+    this.gradientFill2.addColorStop(1, 'rgba(255, 0, 108, 0)');
+
+    this.gradientFill3 = this.ctx.createLinearGradient(0, 0, 0, 300);
+    this.gradientFill3.addColorStop(0, 'rgba(6, 48, 2, 0.8)');
+    this.gradientFill3.addColorStop(.3, 'rgba(7, 103, 21, 0.6)');
+    this.gradientFill3.addColorStop(1, 'rgba(0, 255, 124, 0)');
+
+    this.gradientFill4 = this.ctx.createLinearGradient(0, 0, 0, 300);
+    this.gradientFill4.addColorStop(0, 'rgba(208, 200, 24, 0.8)');
+    this.gradientFill4.addColorStop(.3, 'rgba(192, 195, 37, 0.6)');
+    this.gradientFill4.addColorStop(1, 'rgba(255, 231, 0, 0)');
+
+    setTimeout(() => {
+
+      this.chart.push(new Chart(this.ctx, {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: this.chartData1,
+              label: 'Level 1',
+              backgroundColor: this.gradientFill1,
+              borderColor: 'rgba(128, 182, 244, 0.8)',
+              borderWidth: 2,
+            },
+            {
+              data: this.chartData2,
+              label: 'Level 2',
+              backgroundColor: this.gradientFill2,
+              borderColor: 'rgba(134, 7, 7, 0.8)',
+              borderWidth: 2,
+            },
+            {
+              data: this.chartData3,
+              label: 'Level 3',
+              backgroundColor: this.gradientFill3,
+              borderColor: 'rgba(6, 48, 2, 0.8)',
+              borderWidth: 2,
+            },
+            {
+              data: this.chartData4,
+              label: 'Level 4',
+              backgroundColor: this.gradientFill4,
+              borderColor: 'rgba(208, 200, 24, 0.8)',
+              borderWidth: 2,
+            },
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          title: {
+            display: true,
+            text: 'Performance Chart',
+            fontColor: 'whitesmoke'
+          },
+          legend: {
+            labels: {
+              fontColor: "whitesmoke"
+            }
+          },
+          animation: {
+            easing: 'easeInOutBack'
+          },
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                parser: 'timeFormat',
+                tooltipFormat: 'YYYY.MM.DD HH:mm',
+                displayFormats: {
+                  'day': 'YYYY.MM.DD'
+                }
+              },
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Date',
+                fontColor: 'whitesmoke'
+              },
+              ticks: {
+                fontColor: 'whitesmoke'
+              }
+            }],
+            yAxes: [{
+              type: 'linear',
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'points',
+                fontColor: 'whitesmoke'
+              },
+              ticks: {
+                fontColor: 'whitesmoke'
+              }
+            }]
+          },
+          plugins: {
+            datalabels: {
+              anchor: 'end',
+              align: 'end',
+              font: {
+                size: 20,
+              }
+            },
+            pan: {
+              enabled: true,
+              mode: "x",
+              speed: 10,
+              threshold: 10
+            },
+            zoom: {
+              enabled: true,
+              drag: false,
+              mode: "xy",
+              limits: {
+                max: 10,
+                min: 0.5
+              }
+            }
+          },
+        }
+      }));
+
+    }, 1000);
+
+    /*
+*/
   }
 
 }
